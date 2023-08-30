cf5ef8d908dad2593b6973462a504a2a4500a25d
dcortesnet <dcortes.net@gmail.com>
2023-08-29 20:45:48 -0400

    Clean repo

--- strings/capitalize/capitalize.py
+++
@@ -1,7 +0,0 @@
-def capitalize(words: str):
-    words_list = words.split(" ")
-    words_list_capitalized = list(map(lambda x: x.capitalize(), words_list))
-    words_capitalized = " ".join(words_list_capitalized)
-    return words_capitalized
-
-print(capitalize("chris alan"))
